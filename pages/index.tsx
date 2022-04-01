0d13aa0110c809f634ecd01db3da990a0be0d992
Rickvian Aldi <rickvianaldi@gmail.com>
2022-04-01 14:42:20 +0700

    merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,9 @@ import type { Next } from "next";
 
 import Link from "next/link";
 
-<<<<<<< HEAD
-const pagesBasic1 = ["useStatePageWrong1", "useStatePageWrong2"];
-const pagesBasic2 = ["useStatePageCorrect", "useEffectPage"];
-const pagesMemo = ["useMemoPage", "useCallbackPage"];
-=======
 const pagesBasic1 = ["useStateWrong1", "useStateWrong2", "useStateCorrect"];
 const pagesBasic2 = ["useEffect", "useRef"];
 const pagesMemo = ["useMemo"];
->>>>>>> 49499c6dd46544d02f4b73ad2e9b9b4914d126ce
 
 const pagesCustom = ["useKeyPress", "useToggle"];
 
@@ -33,23 +27,6 @@ const Home: Next = () => {
     <>
       <div className="flex flex-col">
         <h2 className="text-white mb-3">Basic Hooks</h2>
-<<<<<<< HEAD
-        <div className="grid grid-cols-2 divide-x text-white rounded-md mb-3">
-          <PagesList pages={pagesBasic1} />
-        </div>
-        <div className="grid grid-cols-2 divide-x text-white rounded-md mb-3">
-          <PagesList pages={pagesBasic2} />
-        </div>
-
-        <h2 className="text-white mb-3">Custom Hooks</h2>
-        <div className="grid grid-cols-2 divide-x text-white rounded-md">
-          <PagesList pages={pagesMemo} />
-        </div>
-
-        <h2 className="text-white mb-3">Custom Hooks</h2>
-        <div className="grid grid-cols-2 divide-x text-white rounded-md">
-          <PagesList pages={pagesCustom} />
-=======
         <div className="grid grid-cols-4 divide-x text-white rounded-md mb-3">
           <List pages={pagesBasic1} />
         </div>
@@ -64,7 +41,6 @@ const Home: Next = () => {
         <h2 className="text-white mb-3">Custom Hooks</h2>
         <div className="grid grid-cols-4 divide-x text-white rounded-md">
           <List pages={pagesCustom} />
->>>>>>> 49499c6dd46544d02f4b73ad2e9b9b4914d126ce
         </div>
       </div>
     </>
